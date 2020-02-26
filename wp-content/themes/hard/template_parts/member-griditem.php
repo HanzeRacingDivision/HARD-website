50941126f0dbc0847d94d417ddd8c59c8eb099c8
Mark <markeasting@gmail.com>
2020-02-26 15:31:39 +0100

    Added The Stig as default member profile picture

--- wp-content/themes/hard/template_parts/member-griditem.php
+++ wp-content/themes/hard/template_parts/member-griditem.php
@@ -4,10 +4,15 @@ $roles = get_the_terms( $post, 'roles' );
 
 $role = !empty( $roles ) ? array_pop( $roles )->name : 'Member';
 
+$profile_pic = has_post_thumbnail()
+    ? get_the_post_thumbnail_url( $post, 'medium' ) 
+    : get_stylesheet_directory_uri() . '/img/stig.jpg';
+
 ?>
 
 <div class="member">
-    <img src="<?php echo get_the_post_thumbnail_url( $post, 'medium' ); ?>" alt="">
+
+    <img src="<?php echo $profile_pic; ?>">
     
     <p class="name"><?php the_title(); ?></p>
     
