7f0084a6135351e0df79a67b48fd26489ace7a27
Mark <markeasting@gmail.com>
2020-04-08 17:15:48 +0200

    Many minor responsive tweaks

--- wp-content/themes/hard/template_parts/header-content.php
+++ wp-content/themes/hard/template_parts/header-content.php
@@ -8,9 +8,11 @@ $field_location = is_archive() ? $q_object->name : $post->ID;
 
 $cta = get_field('cta_btn', $field_location);
 
+$is_empty = (empty(get_field('intro_text', $field_location)) && empty($cta['url']));;
+
 ?>
 
-<section class="header-content maxwidth">
+<section class="header-content maxwidth <?php if($is_empty) echo 'empty'; ?>">
     
     <h1 class="capitalize"><?php the_field('page_title', $field_location); ?></h1>
     
